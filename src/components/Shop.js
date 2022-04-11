079f8f886916406a79911c9c7e2cc6ee5fc964e2
ZacheryShawne <zachery.shawne@gmail.com>
2022-04-11 12:18:47 -0400

    Shop nav popup styled, not interactive

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSortDown } from 'react-icons/bs'
 import Data from '../Data'
 import ShopCard from './Shop/ShopCard';
+import ShopFilter from './Shop/ShopFilter';
+import '../styles/Shop.scss'
 
 const Shop = () => {
+
+  const [showFilter, setShowFilter] = useState(false)
 
   const shellElements = Data.map(shell => {
     return <ShopCard data={shell} />
   })
 
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 770) {
+      setShowFilter(false)
+    }
+  })
+
+  const toggle = () => {
+    setShowFilter(prev => !prev)
+  }
+
   return (
     <main className='px-4 flex flex-col items-center mx-auto pt-8' >
       <h1 className='text-center text-xl font-black tracking-wide pb-3' >Sea Shells</h1>
       <div className="mobile-sort flex justify-between w-full pb-4">
-        <div className="mobile-sort-left flex items-center ">
+        <div 
+        className="mobile-sort-left flex items-center"
+        onClick={toggle}
+        >
           <BsSortDown className='hover:fill-red-600 cursor-pointer transition-all duration-300' />
           <span className='text-xs tracking-wider pl-1 font-black hover:text-red-600 cursor-pointer transition-all duration-300 hover:underline' >Filter & Sort</span>
         </div>
@@ -21,10 +38,13 @@ const Shop = () => {
           <span className='text-xs tracking-wider font-black' >28 Results</span>
         </div>
       </div>
-      <div className="shop-wrapper
-        grid grid-cols-2 gap-4
-      ">
-        {shellElements}
+      <div className="shop-wrapper w-full">
+        <ShopFilter filter={showFilter} toggle={toggle}/>
+        <div className="shop-elements-wrapper
+          grid grid-cols-2 gap-4 w-full p-4
+        ">
+          {shellElements}
+        </div>
       </div>
     </main>
   )
---
+++ src/components/Shop/ShopFilter.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { BsXLg } from 'react-icons/bs'
+
+const ShopFilter = ({ filter, toggle }) => {
+
+  const [showCategory, setShowCategory] = useState(false)
+  const [showSeries, setShowSeries] = useState(false)
+ 
+
+  const show = {
+    'visibility': 'visible',
+    'left': '0',
+    'transition': 'visibility linear,left .5s ease-in-out',
+  }
+
+  const hide = {
+    'visibility': 'hidden',
+    'left': '-100vw',
+    'transition': 'visibility .5s linear,left .5s ease-in-out'
+  }
+
+  return (
+    <aside
+      className="shop-filter-wrapper fixed z-50 top-0 w-screen h-screen bg-white p-5 transition overflow-y-scroll tracking-widest"
+      style={filter ? show : hide}
+    >
+      <div className="filter-header flex justify-between items-center pb-6 pr-3">
+        <h1 className='text-lg font-black tracking-wider' >FILTER</h1>
+        <BsXLg className="font-black hover:fill-red-600 cursor-pointer transition-all fill-gray-400" onClick={toggle}></BsXLg>
+      </div>
+      <div className="filter-list border-y border-gray-400">
+        <div
+          className="filter-header flex justify-between cursor-pointer hover:bg-gray-100  pr-2 py-4"
+          onClick={() => setShowCategory(prev => !prev)}
+        >
+          <p className='font-black' >Category</p>
+          <p className='font-black'>{showCategory ? '-' : '+'}</p>
+        </div>
+        {showCategory &&
+          <ul className='pl-5 pb-2 tracking-wide text-sm flex flex-col gap-2' >
+            <li className='cursor-pointer hover:underline' >Shells</li>
+            <li className='cursor-pointer hover:underline' >Shark Teeth</li>
+            <li className='cursor-pointer hover:underline' >Fossils</li>
+            <li className='cursor-pointer hover:underline' >Driftwood</li>
+            <li className='cursor-pointer hover:underline' >Fish?</li>
+          </ul>
+        }
+      </div>
+      <div className="filter-list border-y border-gray-400">
+        <div
+          className="filter-header flex justify-between cursor-pointer hover:bg-gray-100  pr-2 py-4"
+          onClick={() => setShowSeries(prev => !prev)}
+        >
+          <p className='font-black' >Series</p>
+          <p className='font-black'>{showSeries ? '-' : '+'}</p>
+        </div>
+        {showSeries &&
+          <ul className='pl-5 pb-2 tracking-wide font-normal text-sm flex flex-col gap-1' >
+            <li className='cursor-pointer hover:underline' >Tiny</li>
+            <li className='cursor-pointer hover:underline' >Small</li>
+            <li className='cursor-pointer hover:underline' >Medium</li>
+            <li className='cursor-pointer hover:underline' >Large</li>
+            <li className='cursor-pointer hover:underline' >Giant</li>
+          </ul>
+        }
+      </div>
+    </aside>
+  )
+}
+
+export default ShopFilter
